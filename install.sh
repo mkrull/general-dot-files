11a440cdecf62b318d8545198701806ea8deeb67
Matthias Krull <m.krull@uninets.eu>
2017-04-10 09:16:49 +0200

    improve handling of source dirs; add i3 config

--- install.sh
+++ install.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+# This directory path
 DOTFILES=$(pwd)
+# This script name
 NAME=$(basename ${0})
 
-for node in $(ls -I ${NAME} -I Makefile ${DOTFILES}); do
+# Collect all dotfiles ignoring hidden files and directories, the Makefile,
+# and this script
+FILES=$(find . \
+    ! -name ${NAME} \
+    ! -name Makefile \
+    ! -path '*/\.*' \
+    -type f \( ! -iname ".*" \) \
+    -printf '%P\n')
+
+for node in ${FILES}; do
     dest="${HOME}/.${node}"
     src="${DOTFILES}/${node}"
     if [ -e "$dest" ]; then
         continue
     fi
+
+    # Create destination directory path if it does not exist
+    DIRNAME=$(dirname "${dest}")
+    [ -d ${DIRNAME} ] || mkdir -p ${DIRNAME}
 
     ln -s "$src" "$dest"
 done
